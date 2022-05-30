0de13a596af0ed11398a3451d45773296801fa8c
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-05-30 17:11:08 +0100

    basic style and configuration

---
+++ src/App.js
@@ -0,0 +1,16 @@
+export default function App(){
+    return(
+        <main className="mainSquare">
+            <div className="instructionsSquare">
+                <h1 className="titleSquare">Tenzies</h1>
+                <p className="descriptionSquare">
+                    Roll until all dice are the same. 
+                    Click each die to freeze it at its current value between rolls. 
+                </p>
+            </div>
+            <div className="mainSquareGame">
+                
+            </div>
+        </main>
+    )
+}
