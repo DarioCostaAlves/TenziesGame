29858770b0e0602d06aaaf5c561b65deb348f4e3
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-06-07 22:53:08 +0100

    Roll unchecked dices only

--- src/App.js
+++ src/App.js
@@ -6,29 +6,37 @@ export default function App(){
 
     const [dice, setDice] = useState(allNewDice())
 
-    const diceElements = dice.map(function(dice, i){
-        return <Die key={dice.id} value={dice.value} 
-        holdDice={() => holdDice(dice.id)}
+    const diceElements = dice.map(die => (
+        <Die
+            key={die.id}
+            value={die.value}
+            isHeld={die.isHeld}
+            holdDice={() => holdDice(die.id)}
         />
-    })
+    ))
+
+    function generateNewDie(){
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
 
     function allNewDice(){
 
         const newDice = []
         
         for(let i = 0; i < 10; i++){
-            newDice.push({
-                value: Math.ceil(Math.random() * 6),
-                isHeld: false,
-                id: nanoid()
-            })
-        }
-        console.log(newDice)
+            newDice.push(generateNewDie())
+        }        
         return newDice
     }
 
     function rollDice(){
-        setDice(allNewDice())
+        setDice(oldDice => oldDice.map(die => {
+            return die.isHeld ? die : generateNewDie()
+        }))
     }
     // console.log(allNewDice())
     function holdDice(id){
